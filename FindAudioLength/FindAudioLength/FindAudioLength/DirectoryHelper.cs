ac7a9a5de912db5171f36d1cc48150442674a2fd
earneszm <zach.earnest@gmail.com>
2016-12-20 18:04:21 -0800

    Small console app to find the summed duration of all mp3s in a directory (recursively)

---
+++ FindAudioLength/FindAudioLength/FindAudioLength/DirectoryHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAudioLength
+{
+    public class DirectoryHelper
+    {
+        public List<string> lstFilesFound { get; set; } = new List<string>();
+
+        public void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d, "*.mp3"))
+                    {
+                        lstFilesFound.Add(f);
+                    }
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+    }
+}
---
+++ FindAudioLength/FindAudioLength/FindAudioLength/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindAudioLength
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Prints the total mp3 duration of all mp3 contained within a directory, traversing recursively.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            var helper = new DirectoryHelper();
+
+            helper.DirSearch(@"E:\Recording");
+
+            double totalDuration = 0;
+
+            foreach (var item in helper.lstFilesFound)
+            {
+                TagLib.File f = TagLib.File.Create(item, TagLib.ReadStyle.Average);
+                totalDuration += (int)f.Properties.Duration.TotalSeconds;
+            }
+
+            Console.WriteLine(string.Format("Total duration in seconds: {0}", totalDuration));
+            Console.WriteLine(string.Format("Total duration in minutes: {0}", totalDuration / 60));
+            Console.WriteLine(string.Format("Total duration in hours: {0}", totalDuration / 60 / 60));
+
+            Console.ReadKey();
+
+
+        }        
+    }
+}
